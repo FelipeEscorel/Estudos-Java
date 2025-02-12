36fc13143ad040c4aaf55d2496c9e547a648fef2
Felipe Escorel <felipeescorel@hotmail.com>
2025-02-12 15:02:32 -0300

    Add files via upload

---
+++ Bitwise/Exemplo01.java
@@ -0,0 +1,22 @@
+package secao7;
+
+import java.util.Scanner;
+
+public class Exemplo01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int mascara = 0b00100000; // Ou 32
+		int n = sc.nextInt();
+	
+		if ((n & mascara) != 0) {
+			System.out.println("O 6° bit é verdadeiro");
+		}
+		else {
+			System.out.println("O 6° bit é falso");
+		}
+		sc.close();
+	}
+
+}
